730b3eb6f323db8b0848855614d202dc196bce77
adexlekunze-dev <adexlekunze@gmail.com>
2025-11-28 23:31:37 +0000

    Add scroll-to-top functionality for product detail pages

---
+++ src/app/products/[sku]/template.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ProductDetailTemplate({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    // Scroll to top when navigating to product detail page
+    window.scrollTo(0, 0);
+  }, []);
+
+  return <>{children}</>;
+}
+
